d151989a87caff51a8d7f3a3f28a4eef8f9a265c
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2025-06-13 21:47:48 -0400

    Add new DTO Class

---
+++ Economy/CurrencyDTO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Data.Economy
+{
+    public sealed class CurrencyDTO
+    {
+        private DateOnly V_Date;
+        private string V_WeekdayName;
+        private float V_Currency;
+
+
+
+        public CurrencyDTO()
+            : base()
+        {
+            this.V_Date = new DateOnly();
+            this.V_WeekdayName = string.Empty;
+            this.V_Currency = 0;
+        }
+
+
+
+        public required DateOnly Date
+        {
+            get => this.V_Date;
+            set => this.V_Date = value;
+        }
+
+        public required string WeekdayName
+        {
+            get => this.V_WeekdayName;
+            set
+            {
+                ArgumentNullException.ThrowIfNullOrEmpty(argument: value);
+                ArgumentNullException.ThrowIfNullOrWhiteSpace(argument: value);
+                
+                this.V_WeekdayName = value;
+            }
+        }
+
+        public required float Currency
+        {
+            get => this.V_Currency;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegativeOrZero<float>(value: value);
+                
+                this.V_Currency = value;
+            }
+        }
+    }
+}
